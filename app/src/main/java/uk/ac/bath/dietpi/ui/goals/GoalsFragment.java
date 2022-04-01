50a2a984dcd7d45a1453f654e372390c7784bc8d
mf809 <mf809@bath.ac.uk>
2022-04-01 14:22:18 +0100

    Added current progress on Goals page. Fixed a few bugs on with the UI.

--- app/src/main/java/uk/ac/bath/dietpi/ui/goals/GoalsFragment.java
+++ app/src/main/java/uk/ac/bath/dietpi/ui/goals/GoalsFragment.java
@@ -1,5 +1,7 @@
 package uk.ac.bath.dietpi.ui.goals;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,39 +12,99 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
 
-import uk.ac.bath.dietpi.R;
+import java.util.Hashtable;
+
+import uk.ac.bath.dietpi.DBHandler;
+import uk.ac.bath.dietpi.MainActivity;
 import uk.ac.bath.dietpi.databinding.FragmentGoalsBinding;
 
 public class GoalsFragment extends Fragment {
 
     private FragmentGoalsBinding binding;
+    private EditText editTextGoal;
+    private Button btnChangeGoal;
+    private TextView textDisplayGoal;
+    private TextView displayProgressTextView;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String TEXT = "text";
+
+    private String savedText;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        GoalsViewModel goalsViewModel =
-                new ViewModelProvider(this).get(GoalsViewModel.class);
 
         binding = FragmentGoalsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        EditText editTextGoal = binding.editTextGoal;
-        Button btnChangeGoal = binding.btnChangeGoal;
-        TextView textDisplayGoal = binding.textDisplayGoal;
+        editTextGoal = binding.editTextGoal;
+        btnChangeGoal = binding.btnChangeGoal;
+        textDisplayGoal = binding.textDisplayGoal;
+        displayProgressTextView = binding.displayProgressTextView;
 
         btnChangeGoal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String newGoal = editTextGoal.getText().toString();
-                textDisplayGoal.setText(newGoal);
+                if(!newGoal.equals(""))
+                {
+                    textDisplayGoal.setText(newGoal + " Calories");
+                    editTextGoal.setText("");
+                    saveData();
+                }
             }
         });
 
-        final TextView textView = binding.textView;
-        goalsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        loadData();
+        displayCurrentProgress();
 
         return root;
+    }
+
+    public void displayCurrentProgress()
+    {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        DBHandler dbHandler = ((MainActivity) getActivity()).getDbHandler();
+
+        Hashtable<String,Float> hT = dbHandler.retrieveTotal();
+
+        String calorie_count = hT.get("Calories").toString();
+
+        /*
+        if(!calorie_count.equals(""))
+        {
+            Integer integer_calorie_count = Integer.parseInt(calorie_count);
+
+
+            String currentGoal = textDisplayGoal.getText().toString().replace(" Calories", "");
+            Integer integerGoal = Integer.parseInt(currentGoal);
+            Integer percentage = Math.round(integerGoal/integer_calorie_count);
+        }
+        */
+
+        displayProgressTextView.setText(calorie_count + " Calories");
+
+    }
+
+    public void saveData()
+    {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(TEXT, textDisplayGoal.getText().toString());
+
+        editor.apply();
+    }
+
+    public void loadData()
+    {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        savedText = sharedPreferences.getString(TEXT, "");
+        textDisplayGoal.setText(savedText);
     }
 
     @Override
