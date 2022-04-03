1f29b285e3717600370b74f5c0b1b239da66e7eb
mf809 <mf809@bath.ac.uk>
2022-04-03 15:47:32 +0100

    Added a dropdown menu for macronutrients and updated code.

--- app/src/main/java/uk/ac/bath/dietpi/ui/goals/GoalsFragment.java
+++ app/src/main/java/uk/ac/bath/dietpi/ui/goals/GoalsFragment.java
@@ -1,11 +1,15 @@
 package uk.ac.bath.dietpi.ui.goals;
 
+import android.app.KeyguardManager;
 import android.content.Context;
+import android.content.ContentValues;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,6 +23,7 @@ import java.util.Hashtable;
 
 import uk.ac.bath.dietpi.DBHandler;
 import uk.ac.bath.dietpi.MainActivity;
+import uk.ac.bath.dietpi.R;
 import uk.ac.bath.dietpi.databinding.FragmentGoalsBinding;
 
 public class GoalsFragment extends Fragment {
@@ -28,12 +33,21 @@ public class GoalsFragment extends Fragment {
     private Button btnChangeGoal;
     private TextView textDisplayGoal;
     private TextView displayProgressTextView;
+    private AutoCompleteTextView autoCompleteTextView;
 
     public static final String SHARED_PREFS = "sharedPrefs";
     public static final String TEXT = "text";
 
     private String savedText;
+    private String selectedMacronutrient;
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        String[] macronutrients = getResources().getStringArray(R.array.macronutrients);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(requireContext(), R.layout.dropdown_item, macronutrients);
+        binding.autoCompleteTextView.setAdapter(arrayAdapter);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -45,6 +59,10 @@ public class GoalsFragment extends Fragment {
         btnChangeGoal = binding.btnChangeGoal;
         textDisplayGoal = binding.textDisplayGoal;
         displayProgressTextView = binding.displayProgressTextView;
+        autoCompleteTextView = binding.autoCompleteTextView;
+
+        selectedMacronutrient = autoCompleteTextView.getText().toString();
+
 
         btnChangeGoal.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,9 +70,11 @@ public class GoalsFragment extends Fragment {
                 String newGoal = editTextGoal.getText().toString();
                 if(!newGoal.equals(""))
                 {
-                    textDisplayGoal.setText(newGoal + " Calories");
+                    selectedMacronutrient = autoCompleteTextView.getText().toString();
+                    textDisplayGoal.setText(newGoal + " " + selectedMacronutrient);
                     editTextGoal.setText("");
                     saveData();
+                    displayCurrentProgress();
                 }
             }
         });
@@ -71,8 +91,28 @@ public class GoalsFragment extends Fragment {
         DBHandler dbHandler = ((MainActivity) getActivity()).getDbHandler();
 
         Hashtable<String,Float> hT = dbHandler.retrieveTotal();
+        String calorie_count;
 
-        String calorie_count = hT.get("Calories").toString();
+        if(selectedMacronutrient.equals("Calories (kcal)"))
+        {
+            calorie_count = hT.get("Calories").toString();
+        }
+        else if(selectedMacronutrient.equals("Fat (g)"))
+        {
+            calorie_count = hT.get("Fat").toString();
+        }
+        else if(selectedMacronutrient.equals("Protein (g)"))
+        {
+            calorie_count = hT.get("Protein").toString();
+        }
+        else if(selectedMacronutrient.equals("Carbs (g)"))
+        {
+            calorie_count = hT.get("Carbohydrates").toString();
+        }
+        else
+        {
+            calorie_count = "";
+        }
 
         /*
         if(!calorie_count.equals(""))
@@ -86,7 +126,7 @@ public class GoalsFragment extends Fragment {
         }
         */
 
-        displayProgressTextView.setText(calorie_count + " Calories");
+        displayProgressTextView.setText(calorie_count + " " + selectedMacronutrient);
 
     }
 
