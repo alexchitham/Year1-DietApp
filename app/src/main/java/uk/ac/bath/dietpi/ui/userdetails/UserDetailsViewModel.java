ca5707fb05949fb5d79ec4af434b580bae6bc10e
Robert-Dobson <43008203+Robert-Dobson@users.noreply.github.com>
2022-04-17 17:51:25 +0100

    Added functionality to user details page: - Added new table to database called tbl_user_details, this contains all the user details including weight and date (so it could also be used to track weight). - Created new methods in DBHandler to insert new user details and to get the current user details - Fixed bugs with the UI - You can enter user details and update them with the press of a button, an error message will appear if there's an issue with the data. - User details will be autopopulated with the latest user details in the database when you first launch the app unless there's no details at all in the database.

--- app/src/main/java/uk/ac/bath/dietpi/ui/userdetails/UserDetailsViewModel.java
+++ app/src/main/java/uk/ac/bath/dietpi/ui/userdetails/UserDetailsViewModel.java
@@ -1,8 +1,14 @@
 package uk.ac.bath.dietpi.ui.userdetails;
 
+import android.graphics.Color;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import uk.ac.bath.dietpi.R;
 
 public class UserDetailsViewModel extends ViewModel {
 
@@ -16,5 +22,15 @@ public class UserDetailsViewModel extends ViewModel {
         return mText;
     }
 
+    public void setErrorMessage(View v, String str, Boolean isError) {
+        TextView textView = v.getRootView().findViewById(R.id.textViewError);
 
+        if (isError) {
+            textView.setTextColor(Color.parseColor("#cf000f"));
+        } else {
+            textView.setTextColor(Color.parseColor("#009944"));
+        }
+
+        textView.setText(str);
+    }
 }
